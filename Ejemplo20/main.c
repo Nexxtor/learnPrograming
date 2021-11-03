3fc4a45991b5fc336fd20e8b8c255200368c6c54
Nestor Aldana <nestor.aldana1@gmail.com>
2021-11-03 14:08:32 -0600

    Add display status for Memory board

--- Ejemplo20/main.c
+++ Ejemplo20/main.c
@@ -8,24 +8,31 @@
 #define DEFAULT_COLUMNS 5
 #define COLOR_STRING_SIZE 12
 
+enum showStatus { HIDDEN, SHOW, MATCH };
+
 struct size {
     int n;
     int m;
 };
 
+struct cell {
+    char card;               // 'A' 'B' 'C'
+    enum showStatus status;  // Si esta baca arribo o boca abajo
+};
+
 struct board {
     struct size dimensions;
     char borderColor[COLOR_STRING_SIZE];
-    char contentColor[COLOR_STRING_SIZE]; 
-    char **content;
+    char contentColor[COLOR_STRING_SIZE];
+    struct cell **content;
 };
 
-// Prototipos, declaración de funciones
 void showParameters(int, const char *[]);
 struct size getBoardSize(int, const char *[]);
 void initBoard(struct board *);
 void freeBoard(struct board *);
 void displayBoard(struct board *);
+void displayCell(char *, char *, struct cell);
 
 int main(int argc, const char *argv[]) {
     showParameters(argc, argv);
@@ -41,7 +48,6 @@ int main(int argc, const char *argv[]) {
     return 0;
 }
 
-// Definicion de funciones
 
 /**
  * Muestra las cadenas de texto guardas en @param argv,
@@ -95,30 +101,32 @@ void initBoard(struct board *board) {
     int cRows = board->dimensions.n;
     int cColumns = board->dimensions.m;
     printf("Llenando el tablero %d x%d", cRows, cColumns);
-    
+
     // Reservando espacio de memoria para las filas
-    board->content = (char **) malloc(sizeof(char *) * cRows);
+    board->content = (struct cell **)malloc(sizeof(struct cell *) * cRows);
     if (board->content != NULL) {
-        for(int i = 0; i < cRows; i++) {
-            (board->content)[i] = (char *) malloc(sizeof(char) * cColumns );
+        for (int i = 0; i < cRows; i++) {
+            (board->content)[i] =
+                (struct cell *)malloc(sizeof(struct cell) * cColumns);
             if ((board->content)[i] == NULL) {
                 printf("No se puede reservar memoria para el tablero");
                 exit(1);
             }
-            for(int j = 0; j< cColumns; j++) {
-                board->content[i][j] = 'A';
+            for (int j = 0; j < cColumns; j++) {
+                // TODO: Generar parejas y posicinarlas de forma aletorias en la matriz
+                board->content[i][j].card = 'A';
+                board->content[i][j].status = MATCH;
             }
         }
     } else {
         printf("No se puede reservar memoria para el tablero");
         exit(1);
     }
-
 }
 
 void freeBoard(struct board *board) {
     int cRows = board->dimensions.n;
-    for(int i = 0 ;i < cRows; i++ ) {
+    for (int i = 0; i < cRows; i++) {
         free((board->content)[i]);
     }
     free(board->content);
@@ -138,7 +146,9 @@ void displayBoard(struct board *board) {
     for (int i = 0; i < board->dimensions.n - 1; i++) {
         // content
         for (int j = 0; j < board->dimensions.m; j++) {
-            printf("│%s %c %s",board->contentColor, board->content[i][j], board->borderColor);
+            char *contentColor = board->contentColor;
+            char *borderColor = board->borderColor;
+            displayCell(borderColor, contentColor, board->content[i][j]);
         }
         // divider
         printf("│\n├");
@@ -149,12 +159,41 @@ void displayBoard(struct board *board) {
     }
     // last line content
     for (int j = 0; j < board->dimensions.m; j++) {
-        printf("│%s %c %s",board->contentColor, board->content[board->dimensions.n -1][j], board->borderColor);
+        char *contentColor = board->contentColor;
+        char *borderColor = board->borderColor;
+        displayCell(borderColor, contentColor,
+                    board->content[board->dimensions.n - 1][j]);
     }
     // Bottom decorator
     printf("│\n└");
-     for (int j = 0; j < board->dimensions.m - 1; j++) {
+    for (int j = 0; j < board->dimensions.m - 1; j++) {
         printf("───┴");
     }
     printf("───┘\033[0m\n\n");
+}
+
+/**
+ * Muestra el contenido de una celda del tablero de memoria
+ * de acuerdo al estado en el que se encuentra */
+void displayCell(char *borderColor, char *contentColor, struct cell cell) {
+    char *hiddenSymbol = "♠"; // Se muestra cuan la carta esta oculta
+    char content[2];
+    content[0] = cell.card; content[1] = '\0';  // Contenido de la carte en formato cadena
+    char *matchString = " "; // Se muesta cuando se ha encontrado la pareja
+
+    // Seleccionando que cadena de text mostar
+    char *displayString; 
+    switch (cell.status){
+        case HIDDEN:
+            displayString = hiddenSymbol;
+            break;
+        case SHOW:
+            displayString = content;
+            break;
+        case MATCH:
+            displayString =  matchString;
+            break;
+    }
+    // Mostrando celda
+    printf("│%s %s %s", contentColor, displayString ,borderColor);
 }
