dcc33133f5e383b2aa3fe1932c5da91644ed828d
Nestor Aldana <nestor.aldana1@gmail.com>
2021-11-08 15:01:03 -0600

    Add color ASCII

---
+++ Ejemplo20/color.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    printf("\033[0;35m Hola \033[1;31m Mundo \033[0m!!!!");
+    printf("┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬");
+    return 0;
+}
